65c209992649ef4dd49cb7f69b3868919d989a21
Heleno Cardoso <helenocardosofilho@gmail.com>
2025-09-16 09:35:10 -0300

    Java POO appusersts 16Set2025 ...

--- 02 Exercises/backend/2025.2/05 - 15Set2025/09 criar arquivo import - inserts.sql
+++ 02 Exercises/backend/2025.2/05 - 15Set2025/09 criar arquivo import - inserts.sql
@@ -1,7 +1,7 @@
-INSERT INTO tb_user (name, idade) VALUES ('Juju Cardoso');
-INSERT INTO tb_user (name, idade) VALUES ('Josy Cardoso');
-INSERT INTO tb_user (name, idade) VALUES ('Maria Santos');
-INSERT INTO tb_user (name, idade) VALUES ('Paulo Nascimento');
-INSERT INTO tb_user (name, idade) VALUES ('Antidio Pinna');
-INSERT INTO tb_user (name, idade) VALUES ('Heleno Cardoso');
-INSERT INTO tb_user (name, idade) VALUES ('Bebel Cardoso');
+INSERT INTO tb_user (nome, idade) VALUES ('Juju Cardoso', 20);
+INSERT INTO tb_user (nome, idade) VALUES ('Josy Cardoso', 54);
+INSERT INTO tb_user (nome, idade) VALUES ('Maria Santos', 72);
+INSERT INTO tb_user (nome, idade) VALUES ('Paulo Nascimento', 57);
+INSERT INTO tb_user (nome, idade) VALUES ('Antidio Pinna', 55);
+INSERT INTO tb_user (nome, idade) VALUES ('Heleno Cardoso', 56);
+INSERT INTO tb_user (nome, idade) VALUES ('Bebel Cardoso', 3);
