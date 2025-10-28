ecdfb02d5a2fe950ee83195b5dce2921c20fbbee
Heleno Cardoso <helenocardosofilho@gmail.com>
2025-10-28 14:27:53 -0300

    properties 28102025

---
+++ 02 Exercises/backend/2025.2/08 - 20Out2025/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_user (nome, idade) VALUES ('Juju Cardoso', 20);
+INSERT INTO tb_user (nome, idade) VALUES ('Josy Cardoso', 54);
+INSERT INTO tb_user (nome, idade) VALUES ('Maria Santos', 72);
+INSERT INTO tb_user (nome, idade) VALUES ('Paulo Nascimento', 57);
+INSERT INTO tb_user (nome, idade) VALUES ('Antidio Pinna', 55);
+INSERT INTO tb_user (nome, idade) VALUES ('Heleno Cardoso', 56);
+INSERT INTO tb_user (nome, idade) VALUES ('Bebel Cardoso', 3);
